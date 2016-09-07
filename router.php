e52813e15db13d9ebd2c167c866a6a84d5342672
Haiming Guo <haming.guo@superd3d.com>
2016-09-07 15:51:14 +0800

    text-align:right

--- router.php
+++ router.php
@@ -1,14 +1,11 @@
 <?php
 
 function main(){
-	error_log("router.php\r\n", 3, './log.txt');
     $mysql_server_name='mysql:host=localhost;dbname=blog;port=3306';
-    $mysql_username="blog"; 
-    $mysql_password="guoguo"; 
-         
+    $mysql_username="blog";
+    $mysql_password="guoguo";
     $pdo = new PDO($mysql_server_name,$mysql_username, $mysql_password);
 	$pdo->exec('set names utf8');
-
 
 	$pathinfo = explode("/", $_SERVER['PATH_INFO']);
 
@@ -16,7 +13,6 @@ function main(){
 	if($location == ''){
 		header("Location: http://localhost/index.html");
 	} elseif ($location == 'GetCategory') {
-		error_log("router.php:: GetCategory\r\n", 3, './log.txt');
 		$qresult = $pdo->query("select name from category");
 		if(!qresult){
 
@@ -25,7 +21,7 @@ function main(){
 				$results[] = $row[0];
 			}
 		}
-		
+
 		$ret = json_encode($results);
 
 		header('Content-Type:application/octet-stream');
